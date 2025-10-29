6a78d726b89728a27a9bac9261c50f1df59d805c
Pawan Singh <pawansinghrishu@gmail.com>
2025-10-29 17:18:47 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
 yum install java-17-amazon-corretto -y
-wget https://downloads.apache.org/tomcat/tomcat-11/v11.0.12/bin/apache-tomcat-11.0.12.tar.gz
-tar -zxvf apache-tomcat-11.0.12.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.12/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.12/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.12/conf/tomcat-users.xml
+wgethttps://downloads.apache.org/tomcat/tomcat-11/v11.0.13/bin/apache-tomcat-11.0.13.tar.gz
+tar -zxvf apache-tomcat-11.0.13.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-11.0.13/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-11.0.13/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="root123" roles="manager-gui, manager-script"/>' apache-tomcat-11.0.13/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-11.0.12/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-11.0.12/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-11.0.12/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-11.0.12/webapps/manager/META-INF/context.xml
-sh apache-tomcat-11.0.12/bin/startup.sh
+sed -i '56d' apache-tomcat-11.0.13/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-11.0.13/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-11.0.13/webapps/manager/META-INF/context.xml
+sh apache-tomcat-11.0.13/bin/startup.sh
