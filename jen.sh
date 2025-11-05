87142ef028166c9b7397263a1d9f886ec825a7f3
Pawan Singh <pawansinghrishu@gmail.com>
2025-11-05 21:08:46 +0530

    ubuntu
    This script installs OpenJDK 17 and Jenkins on a Debian-based system.

---
+++ jen.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt install openjdk-17-jre-headless -y
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
